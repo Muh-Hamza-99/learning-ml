d165678df5809e1762c678a8624d5e6757372a7c
muhammad-hamza <Muh-Hamza-99>
2022-08-26 15:47:55 +0300

    Commenting out compute-intensive visualisations for classification algorithms

--- 11-support-vector-machine/main.py
+++ 11-support-vector-machine/main.py
@@ -44,22 +44,22 @@ print(np.concatenate((y_predicted.reshape(len(y_predicted), 1), y_test.reshape(l
 confusion_matrix = confusion_matrix(y_test, y_predicted)
 accuracy_score = accuracy_score(y_test, y_predicted)
 
-# Visualising the training set results
+# Visualising the training set results (compute-intensive visualisation)
 
-x_set, y_set = standard_scaler.inverse_transform(x_train), y_train
-x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min()-10, stop=x_set[:, 0].max()+10, step=0.25), np.arange(start=x_set[:, 1].min()-1000, stop=x_set[:, 1].max()+1000, step=0.25))
-plt.contourf(x1, x2, classifier.predict(standard_scaler.transform(np.array([x1.ravel(), x2.ravel()]).T)).reshape(x1.shape), alpha=0.75, cmap=ListedColormap(("red", "green")))
-plt.xlim(x1.min(), x1.max())
-plt.ylim(x2.min(), x2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(x_set[y_set==j, 0], x_set[y_set==j, 1], c=ListedColormap(("red", "green"))(i), label=j)
-plt.title("Linear Support Vector Machine (Training set)")
-plt.xlabel("Age")
-plt.ylabel("Estimated Salary")
-plt.legend()
-plt.show()
+# x_set, y_set = standard_scaler.inverse_transform(x_train), y_train
+# x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min()-10, stop=x_set[:, 0].max()+10, step=0.25), np.arange(start=x_set[:, 1].min()-1000, stop=x_set[:, 1].max()+1000, step=0.25))
+# plt.contourf(x1, x2, classifier.predict(standard_scaler.transform(np.array([x1.ravel(), x2.ravel()]).T)).reshape(x1.shape), alpha=0.75, cmap=ListedColormap(("red", "green")))
+# plt.xlim(x1.min(), x1.max())
+# plt.ylim(x2.min(), x2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(x_set[y_set==j, 0], x_set[y_set==j, 1], c=ListedColormap(("red", "green"))(i), label=j)
+# plt.title("Linear Support Vector Machine (Training set)")
+# plt.xlabel("Age")
+# plt.ylabel("Estimated Salary")
+# plt.legend()
+# plt.show()
 
-# Visualising the test set results
+# Visualising the test set results (compute-intensive visualisation)
 
 # x_set, y_set = standard_scaler.inverse_transform(x_test), y_test
 # x1, x2 = np.meshgrid(np.arange(start=x_set[:, 0].min()-10, stop=x_set[:, 0].max()+10, step=0.25), np.arange(start=x_set[:, 1].min()-1000, stop=x_set[:, 1].max()+1000, step=0.25))
